95f5713cd4e202d3998d64c53f3d0c842740b18c
Sohail Selky <sohail.selky@ibm.com>
2022-06-19 16:54:02 -0500

    added flag to fs command

--- app/deploy/deploy.js
+++ app/deploy/deploy.js
@@ -2,19 +2,6 @@ import { deployBundle } from "@tiny-frontend/deploy-cloudflare";
 import dotenv from "dotenv";
 import { readFile } from "fs/promises";
 
-const fs = require('fs');
-
-const content = "declare module '@elyra/canvas'";
-// echo "declare module '@elyra/canvas';" >> node_modules/@elyra/canvas/dist/common-canvas.d.ts
-try {
-  console.log("Patching in @elya/canvas module declaration ..")
-  fs.writeFileSync('../node_modules/@elyra/canvas/dist/common-canvas.d.ts', content);
-  console.log("Module declaration complete!")
-
-} catch (err) {
-  console.error(err);
-}
-
 const contractPackageJsonPath = new URL(
   "../../contract/package.json",
   import.meta.url
--- app/deploy/patch.js
+++ app/deploy/patch.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+import fs from 'fs'
 
 const content = "declare module '@elyra/canvas'";
 // echo "declare module '@elyra/canvas';" >> node_modules/@elyra/canvas/dist/common-canvas.d.ts
 try {
   console.log("Patching in @elya/canvas module declaration ..")
-  fs.writeFileSync('../node_modules/@elyra/canvas/dist/common-canvas.d.ts', content);
+  fs.writeFileSync('../node_modules/@elyra/canvas/dist/common-canvas.d.ts', content, { flag: 'w+' });
   console.log("Module declaration complete!")
 
 } catch (err) {

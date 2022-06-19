9fd5bde25106159803972e71742cc92ffc4e6326
Sohail Selky <sohail.selky@ibm.com>
2022-06-19 17:00:09 -0500

    changed patch to path to correct location from the 'app' folder

--- app/deploy/patch.js
+++ app/deploy/patch.js
@@ -4,7 +4,7 @@ const content = "declare module '@elyra/canvas'";
 // echo "declare module '@elyra/canvas';" >> node_modules/@elyra/canvas/dist/common-canvas.d.ts
 try {
   console.log("Patching in @elya/canvas module declaration ..")
-  fs.writeFileSync('../node_modules/@elyra/canvas/dist/common-canvas.d.ts', content, { flag: 'w+' });
+  fs.writeFileSync('node_modules/@elyra/canvas/dist/common-canvas.d.ts', content, { flag: 'w+' });
   console.log("Module declaration complete!")
 
 } catch (err) {
